c6ed1ed11b39690496f7b5a99d606bd1a6bc6496
mquinlan81 <m.quinlan81@gmail.com>
2025-04-01 15:00:52 -0500

    updated with book editing capabilities

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const myLibrary = [
     author: 'Dr. Bob Rotella',
     pages: '501',
     read: true,
+    cover: '',
   },
   {
     id: '9d4c95ee-8e5a-40fc-93cb-0724b2977558',
@@ -28,6 +29,7 @@ const myLibrary = [
     author: 'Ryan Holliday',
     pages: '405',
     read: false,
+    cover: '',
   },
   {
     id: '9b47c856-1dca-42db-bb77-4e13a8d1949d',
@@ -63,11 +65,48 @@ function addBookToLibrary(obj) {
   myLibrary.push(obj);
 }
 
+function showAddBookBtn() {
+  document.getElementById('add-to-library').style.display = 'inline-block';
+  document.getElementById('edit-book').style.display = 'none';
+}
+
+function showEditBookBtn() {
+  document.getElementById('add-to-library').style.display = 'none';
+  document.getElementById('edit-book').style.display = 'inline-block';
+}
+
+function fillForm(obj) {
+  let copiedObj = { ...obj };
+  document.getElementById('title').value = copiedObj.title;
+  document.getElementById('author').value = copiedObj.author;
+  document.getElementById('pages').value = copiedObj.pages;
+  document.getElementById('read').checked = copiedObj.read;
+  document.getElementById('cover-link').value = copiedObj.cover || '';
+
+  let editButton = document.getElementById('edit-book');
+
+  let newEditButton = editButton.cloneNode(true);
+  editButton.parentNode.replaceChild(newEditButton, editButton);
+
+  showEditBookBtn();
+  newEditButton.addEventListener('click', () => editBook(copiedObj));
+}
+
 function displayBookList() {
-  myLibrary.forEach((item) => {
+  document.getElementById('book-list').innerHTML = '';
+  let libraryCopy = myLibrary.sort((a, b) => {
+    const nameA = a.title.toUpperCase();
+    const nameB = b.title.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  libraryCopy.forEach((item) => {
     let bookCard = createBookCard(item);
-    // let bookCard = document.createElement('div');
-    // bookCard.textContent = `${item.title}, ${item.author}, ${item.pages}, ${item.read}`;
     document.getElementById('book-list').appendChild(bookCard);
   });
 }
@@ -109,33 +148,62 @@ function createBookCard(obj) {
   const pages = document.createElement('p');
   pages.textContent = `${obj.pages} pages`;
   const read = document.createElement('p');
-  if (obj.read) {
-    read.setAttribute('class', 'read');
-    read.textContent = 'Read';
-  } else {
-    read.setAttribute('class', 'not-read');
-    read.textContent = 'Not Read';
-  }
+  read.setAttribute('class', obj.read ? 'read' : 'not-read');
+  read.textContent = obj.read ? 'Read' : 'Not Read';
+
+  const clickToEdit = document.createElement('p');
+  clickToEdit.textContent = 'Click card to edit';
+  clickToEdit.style.fontStyle = 'italic';
 
   back.appendChild(backTitle);
   back.appendChild(backAuthor);
   back.appendChild(pages);
   back.appendChild(read);
+  back.appendChild(clickToEdit);
   innerCard.appendChild(front);
   innerCard.appendChild(back);
   bookCard.appendChild(innerCard);
+  bookCard.addEventListener('click', () => fillForm(obj));
   return bookCard;
 }
 
-document.getElementById('add-to-library').addEventListener('click', (e) => {
-  e.preventDefault();
+function editBook(obj) {
+  myLibrary.forEach((item, index) => {
+    if (item.id === obj.id) {
+      myLibrary[index].title = document.getElementById('title').value;
+      myLibrary[index].author = document.getElementById('author').value;
+      myLibrary[index].pages = document.getElementById('pages').value;
+      myLibrary[index].read = document.getElementById('read').checked;
+      myLibrary[index].cover = document.getElementById('cover-link').value;
+    }
+  });
+
+  displayBookList();
+  document.getElementById('book-form').reset();
+  showAddBookBtn();
+}
+
+function createNewBook() {
   let title = document.getElementById('title').value;
   let author = document.getElementById('author').value;
   let pages = document.getElementById('pages').value;
   let read = document.getElementById('read').checked;
+  if (document.getElementById('cover-link')) {
+    let cover = document.getElementById('cover-link').value;
+  } else {
+    let cover = '';
+  }
   let cover = document.getElementById('cover-link').value;
   let newBook = new Book(title, author, pages, read, cover);
   addBookToLibrary(newBook);
+}
+
+document.getElementById('add-to-library').addEventListener('click', (e) => {
+  e.preventDefault();
+  createNewBook();
+  document.getElementById('book-form').reset();
+  showAddBookBtn();
   document.getElementById('book-list').innerHTML = '';
   displayBookList();
+  document.getElementById('title').focus();
 });
